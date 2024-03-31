7b53db447a0ba14b59344f47cc8a7e4ca42b7872
Alexis Merino <alexismerino49@gmail.com>
2024-03-31 12:50:18 -0500

    added prompts for he user and the answer promise

--- index.js
+++ index.js
@@ -3,6 +3,41 @@ const fs = require('fs');
 const generateLogo = require('./lib/genereateLogo');
 
 inquirer
-    .prompts({
-        
+    .prompt([
+        {
+            type:"input", 
+            name:"logoText", 
+            message:"Input three chracters for your logo:",
+            maxLength:3
+        },
+
+        {
+            type:"input", 
+            name:"textColors", 
+            message:"For the logo text, enter a color or hexadecimal number:"
+        },
+
+        {
+            type:"list", 
+            name:"shapes", 
+            choices:[
+                "Circle",
+                "Triangle",
+                "Square",
+            ]
+        }, 
+
+        {
+            type:"input", 
+            name:"shapeColors", 
+            message:"For the shape, enter a color or hexadecimal number:"
+        },
+    ])
+
+    .then ((answers) => {
+        const logoContent = generateLogo(answers); 
+
+        fs.writeFile('./example/logo.svg', logoContent, (err) =>{
+            err ? console.log(err) : console.log("Generated logo.svg")
+        })
     })

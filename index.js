d86999bcf769509ac9dfcdb1eacded4913d859aa
Alexis Merino <alexismerino49@gmail.com>
2024-04-01 11:51:10 -0500

    added init function and generateLogo function

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
+
+// imports inquirer
 const inquirer = require('inquire');
+//imports filesystem to use writeFile
 const fs = require('fs');
+// imports the generate logo function 
 const generateLogo = require('./lib/genereateLogo');
 
-inquirer
-    .prompt([
+// questions prompts that will be presented to the user
+const questions = [
+
+    //the text for the logo
         {
             type:"input", 
             name:"logoText", 
             message:"Input three chracters for your logo:",
-            maxLength:3
+            maxLength:3 // sets the limit of the user input to 3 characters
         },
-
+    // the color for the three characters the user used
         {
             type:"input", 
             name:"textColors", 
             message:"For the logo text, enter a color or hexadecimal number:"
         },
-
+    // the list of shapes for the user to pick 
         {
             type:"list", 
             name:"shapes", 
@@ -26,18 +32,31 @@ inquirer
                 "Square",
             ]
         }, 
-
+    // the color of the shape for the user to pick
         {
             type:"input", 
             name:"shapeColors", 
             message:"For the shape, enter a color or hexadecimal number:"
         },
-    ])
+    ]
 
+
+// function to initialize the application  
+    function init (){
+
+// pass the questions array to inquirer
+     inquirer (questions); 
+
+// the promise for the answers
     .then ((answers) => {
         const logoContent = generateLogo(answers); 
-
+        
+        // writes the svg file and sends out to the example folder 
         fs.writeFile('./example/logo.svg', logoContent, (err) =>{
+            // ternary conditional statement 
             err ? console.log(err) : console.log("Generated logo.svg")
         })
     })
+}
+// calls the init fucntion to start the application.
+init();
